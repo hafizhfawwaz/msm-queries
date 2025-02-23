eccbd25a5cafa5d8e4f1c12375b1d625c1ad5541
Fawwaz Hafizh <100624670+hafizhfawwaz@users.noreply.github.com>
2025-02-23 16:33:13 +0000

    second commit

--- app/controllers/onemovie_controller.rb
+++ app/controllers/onemovie_controller.rb
@@ -2,6 +2,8 @@ class OnemovieController < ApplicationController
 
   def print_one_movie
 
+    @movie_id = params.fetch("movie_id").to_i
+    @movie_info = Actor.where({ :id => "#{@movie_id}" }).at(0)
     render({ :template => "msm_templates/one_movie"})
 
   end
